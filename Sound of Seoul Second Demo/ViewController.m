54b1a5274951ee1c35877228c0859909d9d9e53c
pearlc <jinhochung@Jinhoui-MacBook-Pro.local>
2014-11-16 14:51:40 +0900

    SWRevealViewController 추가

--- Sound of Seoul Second Demo/ViewController.m
+++ Sound of Seoul Second Demo/ViewController.m
@@ -11,6 +11,13 @@
 
 @interface ViewController ()
 
+/**
+ 홍대 : 37.550740, 126.925495
+ 신사동 : 37.517581, 127.019291
+ */
+// 내 위치
+@property(nonatomic,retain) CLLocationManager *locationManager;
+
 @end
 
 @implementation ViewController {
@@ -30,6 +37,10 @@
                                                                  zoom:6];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
+    mapView_.settings.myLocationButton = YES;
+    mapView_.settings.indoorPicker = YES;
+    mapView_.settings.compassButton = YES;
+
     self.view = mapView_;
     
     // Creates a marker in the center of the map.
@@ -38,11 +49,26 @@
     marker.title = @"Sydney";
     marker.snippet = @"Australia";
     marker.map = mapView_;
+    
+    // 내 위치
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters; // 100 m
+    [self.locationManager startUpdatingLocation];
+
+    NSLog(@"User's location: %@", mapView_.myLocation);
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// 내 위치
+- (NSString *)deviceLocation {
+    NSString *theLocation = [NSString stringWithFormat:@"latitude: %f longitude: %f", self.locationManager.location.coordinate.latitude, self.locationManager.location.coordinate.longitude];
+    return theLocation;
 }
 
 @end
